9012689b72812b3b69662b8923fa43936f72a192
BhavyaPatel18 <pate4964@sheridancollege.ca>
2023-04-17 18:12:51 -0500

    edit

--- Resource_Manager.py
+++ Resource_Manager.py
@@ -2,13 +2,21 @@ class ResourceManager:
     def __init__(self, resources):
         self.resources = resources
 
-    def get_all_resources(self,):
+    def get_all_resources(self):
         return self.resources
-
-    def create_resource(self, attributes):  # code to create a new resource and add to resources list
+    
+    def __init__(self): 
+        return self
+    
+    def create_resource(self, attributes): # code to create a new resource and add to resources list
+        self.attributes = attributes
 
     def search_resources(self, search_criteria):  # code to search resources based on search criteria and return matching resources
+        self.search_criteria = search_criteria
 
     def update_resource(self, unique_characteristic, new_attributes):  # code to update matching resource with new attribute values
+        self.unique_characteristic = unique_characteristic
+        self.new_attributes =  new_attributes
 
     def delete_resource(self, unique_characteristic):  # code to delete matching resource
+        self.unique_characteristic = unique_characteristic
